5afadd7e54c51ad7682acf04d95670ac8f4b8a80
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:46:51 +0530

    Update and rename maintenance.py to maintenance.py

--- WtfAno/plugins/sudo/maintenance.py
+++ WtfAno/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
