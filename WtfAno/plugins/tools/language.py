99d88c03a4d8e98c317b49614d3c53131877337f
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:37:02 +0530

    Update and rename language.py to language.py

--- WtfAno/plugins/tools/language.py
+++ WtfAno/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_lang, set_lang
-from AnonXMusic.utils.decorators import ActualAdminCB, language, languageCB
+from WtfAno import app
+from WtfAno.utils.database import get_lang, set_lang
+from WtfAno.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
