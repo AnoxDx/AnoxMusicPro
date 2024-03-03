862d601fdaa8924ac9289f82d317f12ede180f8c
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 10:41:17 +0530

    Update pastebin.py

--- AnonXMusic/utils/pastebin.py
+++ AnonXMusic/utils/pastebin.py
@@ -13,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def AnonyBin(text):
+async def AnoxDxBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
