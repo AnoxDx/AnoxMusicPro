06d27d2e7fcbf242d191f28af02878f17855db52
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 10:56:29 +0530

    Update and rename admins.py to admins.py

--- WtfAno/utils/decorators/admins.py
+++ WtfAno/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from WtfAno import app
+from WtfAno.misc import SUDOERS, db
+from WtfAno.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
