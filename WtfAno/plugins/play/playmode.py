b441e8b3cfe12082cae3f70474140a40a2d15131
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:49:02 +0530

    Update and rename playmode.py to playmode.py

--- WtfAno/plugins/play/playmode.py
+++ WtfAno/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from AnonXMusic.utils.decorators import language
-from AnonXMusic.utils.inline.settings import playmode_users_markup
+from WtfAno import app
+from WtfAno.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from WtfAno.utils.decorators import language
+from WtfAno.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
