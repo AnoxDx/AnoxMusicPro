ed64b7ab8aa5d648880bb27315021e7a63b5b4d8
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 10:35:33 +0530

    Update Telegram.py

--- AnonXMusic/platforms/Telegram.py
+++ AnonXMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from AnonXMusic import app
-from AnonXMusic.utils.formatters import (
+from WtfAno import app
+from WtfAno.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
