11fa49eb92bcb0efa4ea2992a8d1130ce2cb7272
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 16:52:52 +0530

    Update skip.py

--- WtfAno/plugins/admins/skip.py
+++ WtfAno/plugins/admins/skip.py
@@ -74,7 +74,7 @@ async def skip(cli, message: Message, _, chat_id):
                     reply_markup=close_markup(_),
                 )
                 try:
-                    return await Anony.stop_stream(chat_id)
+                    return await AnoxDx.stop_stream(chat_id)
                 except:
                     return
         except:
@@ -85,7 +85,7 @@ async def skip(cli, message: Message, _, chat_id):
                     ),
                     reply_markup=close_markup(_),
                 )
-                return await Anony.stop_stream(chat_id)
+                return await AnoxDx.stop_stream(chat_id)
             except:
                 return
     queued = check[0]["file"]
