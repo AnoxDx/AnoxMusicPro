781bbdf4d1f1b2a400fc2896dd8fd50eca0e67fc
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:47:39 +0530

    Update and rename logger.py to logger.py

--- WtfAno/plugins/sudo/logger.py
+++ WtfAno/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import add_off, add_on
+from WtfAno.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
