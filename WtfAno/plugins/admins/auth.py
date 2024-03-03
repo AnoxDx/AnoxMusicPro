4a9cf9ba1bb7badec3aa139673da82332e4c46c6
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:38:20 +0530

    Update and rename auth.py to auth.py

--- WtfAno/plugins/admins/auth.py
+++ WtfAno/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from WtfAno import app
+from WtfAno.utils import extract_user, int_to_alpha
+from WtfAno.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from WtfAno.utils.decorators import AdminActual, language
+from WtfAno.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
