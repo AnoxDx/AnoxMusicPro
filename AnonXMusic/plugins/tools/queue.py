eb4211f5ed7e7c91ff54e7b58de1f20723d56d58
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:35:49 +0530

    Update queue.py

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from WtfAno import app
+from WtfAno.misc import db
+from WtfAno.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from WtfAno.utils.database import get_cmode, is_active_chat, is_music_playing
+from WtfAno.utils.decorators.language import language, languageCB
+from WtfAno.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
