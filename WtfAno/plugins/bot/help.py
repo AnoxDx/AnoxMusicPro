522072ef2b351cf0e16be5019ced1cd8b9666a42
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:32:03 +0530

    Update and rename help.py to help.py

--- WtfAno/plugins/bot/help.py
+++ WtfAno/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
-from AnonXMusic.utils.database import get_lang
-from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from WtfAno import app
+from WtfAno.utils import help_pannel
+from WtfAno.utils.database import get_lang
+from WtfAno.utils.decorators.language import LanguageStart, languageCB
+from WtfAno.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
