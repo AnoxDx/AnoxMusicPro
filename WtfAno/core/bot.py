79b5c51559428c1715a2bbbc46c776a2d4d86317
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 16:50:33 +0530

    Update bot.py

--- WtfAno/core/bot.py
+++ WtfAno/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Anony(Client):
+class AnoxDx(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
