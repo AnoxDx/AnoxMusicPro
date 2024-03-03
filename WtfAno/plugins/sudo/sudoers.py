dde4c0cca7048ffb50e6113f2ef5d288c03548ca
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:38:29 +0530

    Update and rename sudoers.py to sudoers.py

--- WtfAno/plugins/sudo/sudoers.py
+++ WtfAno/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import add_sudo, remove_sudo
+from WtfAno.utils.decorators.language import language
+from WtfAno.utils.extraction import extract_user
+from WtfAno.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
