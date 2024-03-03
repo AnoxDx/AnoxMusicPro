3ef38281d3d74f4af0263ddfec40c4e01e7af671
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 19:47:05 +0530

    Update help.py

--- WtfAno/plugins/bot/help.py
+++ WtfAno/plugins/bot/help.py
@@ -52,7 +52,7 @@ async def help_com_group(client, message: Message, _):
     
 @app.on_callback_query(filters.regex("Page1"))
 async def callback_query(client, CallbackQuery):
-     await CallbackQuery.edit_message_media(photo = config.REPO_IMG_URL, reply_markup=keyboard)
+     await CallbackQuery.edit_message_media(photo = "https://graph.org/file/513da5a8bd5c39bd1b78f.mp4", reply_markup=keyboard)
 
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
 @languageCB
