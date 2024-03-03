e4a16a83d12eebb58ab50d1cb7984a6e81101f42
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:30:06 +0530

    Update and rename active.py to active.py

--- WtfAno/plugins/tools/active.py
+++ WtfAno/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
