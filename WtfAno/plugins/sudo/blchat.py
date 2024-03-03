d86161b1ff30ca6078f22010a7f9ead6c37dd627
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:42:29 +0530

    Update and rename blchat.py to blchat.py

--- WtfAno/plugins/sudo/blchat.py
+++ WtfAno/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from AnonXMusic.utils.decorators.language import language
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from WtfAno.utils.decorators.language import language
 from config import BANNED_USERS
 
 
