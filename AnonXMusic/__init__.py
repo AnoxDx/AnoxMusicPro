d4588592a0a9d349fbaa644c69c7fb4329b35723
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 09:23:37 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from WtfAno.core.bot import Anony
+from WtfAno.core.dir import dirr
+from WtfAno.core.git import git
+from WtfAno.core.userbot import Userbot
+from WtfAno.misc import dbb, heroku
 
 from .logging import LOGGER
 
