9cfeda3f4b7e69a1512cec491ec9304817f365f6
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 10:58:07 +0530

    Update play.py

--- AnonXMusic/utils/decorators/play.py
+++ AnonXMusic/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from WtfAno import YouTube, app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from AnonXMusic.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from AnonXMusic.utils.inline import botplaylist_markup
+from WtfAno.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
