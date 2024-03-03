21e1a95419a6cff94ebe66724059ff16b51e2714
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:45:34 +0530

    Update and rename resume.py to resume.py

--- WtfAno/plugins/admins/resume.py
+++ WtfAno/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_on
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from WtfAno import app
+from WtfAno.core.call import AnoxDx
+from WtfAno.utils.database import is_music_playing, music_on
+from WtfAno.utils.decorators import AdminRightsCheck
+from WtfAno.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Anony.resume_stream(chat_id)
+    await AnoxDx.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
