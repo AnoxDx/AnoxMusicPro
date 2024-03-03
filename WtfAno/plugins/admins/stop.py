66e250996f20b2c247093b01c6c5ad889647d7e2
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:33:03 +0530

    Update and rename stop.py to stop.py

--- WtfAno/plugins/admins/stop.py
+++ WtfAno/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from WtfAno import app
+from WtfAno.core.call import AnoxDx
+from WtfAno.utils.database import set_loop
+from WtfAno.utils.decorators import AdminRightsCheck
+from WtfAno.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Anony.stop_stream(chat_id)
+    await AnoxDx.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
