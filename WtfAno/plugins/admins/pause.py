24a5de41d99f9033a48787f3180aa9887c7c0334
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:44:35 +0530

    Update and rename pause.py to pause.py

--- WtfAno/plugins/admins/pause.py
+++ WtfAno/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from WtfAno import app
+from WtfAno.core.call import AnoxDx
+from WtfAno.utils.database import is_music_playing, music_off
+from WtfAno.utils.decorators import AdminRightsCheck
+from WtfAno.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anony.pause_stream(chat_id)
+    await AnoxDx.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
