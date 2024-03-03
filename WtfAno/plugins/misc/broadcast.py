950fefdc63c7974f387b14f989bf79ca7a1b1cce
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:53:04 +0530

    Update and rename broadcast.py to broadcast.py

--- WtfAno/plugins/misc/broadcast.py
+++ WtfAno/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from WtfAno import app
+from WtfAno.misc import SUDOERS
+from WtfAno.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from WtfAno.utils.decorators.language import language
+from WtfAno.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
