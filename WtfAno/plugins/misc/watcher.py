95f4530783e9b1f18ba4a34342802565a25feb61
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:53:44 +0530

    Update and rename watcher.py to watcher.py

--- WtfAno/plugins/misc/watcher.py
+++ WtfAno/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from WtfAno import app
+from WtfAno.core.call import Anony
 
 welcome = 20
 close = 30
