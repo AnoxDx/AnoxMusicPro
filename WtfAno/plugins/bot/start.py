5ca1cf3ad101f57fa646210bb62724c91cc1c06d
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 18:55:23 +0530

    Update start.py

--- WtfAno/plugins/bot/start.py
+++ WtfAno/plugins/bot/start.py
@@ -2,7 +2,7 @@ import time
 
 from pyrogram import filters
 from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
