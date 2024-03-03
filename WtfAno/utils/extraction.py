e91e339a4ba5df10a9f6114d5d4a8d238edb5caa
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 10:50:32 +0530

    Update and rename extraction.py to extraction.py

--- WtfAno/utils/extraction.py
+++ WtfAno/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from AnonXMusic import app
+from WtfAno import app
 
 
 async def extract_user(m: Message) -> User:
