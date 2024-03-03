897df4b54a05b859b79e1be6c715c405b24a5901
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 11:50:10 +0530

    Update and rename play.py to play.py

--- WtfAno/plugins/play/play.py
+++ WtfAno/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from WtfAno import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from WtfAno.core.call import Anony
+from WtfAno.utils import seconds_to_min, time_to_seconds
+from WtfAno.utils.channelplay import get_channeplayCB
+from WtfAno.utils.decorators.language import languageCB
+from WtfAno.utils.decorators.play import PlayWrapper
+from WtfAno.utils.formatters import formats
+from WtfAno.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from WtfAno.utils.logger import play_logs
+from WtfAno.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
