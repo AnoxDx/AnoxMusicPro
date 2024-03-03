1f4ac762bf92f7b56a9446e9042100d717927394
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-03 12:28:19 +0530

    Update and rename inline.py to inline.py

--- WtfAno/plugins/bot/inline.py
+++ WtfAno/plugins/bot/inline.py
@@ -5,8 +5,8 @@ from pyrogram.types import (
 )
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
-from AnonXMusic.utils.inlinequery import answer
+from WtfAno import app
+from WtfAno.utils.inlinequery import answer
 from config import BANNED_USERS
 
 
